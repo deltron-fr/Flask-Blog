3a28f31ba9cdd31d351e402d2e32e68605e3f21a
deltron-fr <kayceeogbonnaya2304@gmail.com>
2024-06-22 11:36:47 +0100

    My first Commit

---
+++ Flaskblog.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts =[
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First Blog Post',
+        'date': 'June 22, 2024'
+    },
+    {
+        'author': 'Ogbonnaya Kelechi',
+        'title': 'Blog Post 2',
+        'content': 'Second Blog Post',
+        'date': 'June 22, 2024'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
