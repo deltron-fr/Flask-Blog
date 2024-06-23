64c8d66b096b73a5d6121eac3ce04744e0b6a2cb
deltron-fr <kayceeogbonnaya2304@gmail.com>
2024-06-23 18:15:39 +0100

    Added more features to the blog

--- Flaskblog.py
+++ Flaskblog.py
@@ -1,5 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import Registrationform, Loginform
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 posts =[
     {
@@ -25,6 +35,18 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = Registrationform()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = Loginform()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
